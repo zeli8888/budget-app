bcbf152f00b9b980325b7a1f5f6ceb29ae8a1f5c
zeli8888 <zeli8888@outlook.com>
2026-02-05 11:03:53 +0000

    fix(frontend): add context to config to start with context

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite'; // Provides the configuration schema for better IDE autocompletion
+import { defineConfig, loadEnv } from 'vite'; // Provides the configuration schema for better IDE autocompletion
 import react from '@vitejs/plugin-react'; // Essential plugin to support React (JSX and Fast Refresh)
 import viteTsconfigPaths from 'vite-tsconfig-paths'; // Allows Vite to recognize alias paths from your tsconfig.json
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    // plugins: An array of tools that extend Vite's core functionality
-    plugins: [
-        react(), // Enables React-specific features like Hot Module Replacement (HMR)
-        viteTsconfigPaths() // Ensures imports like '@/components/Button' work correctly
-    ],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        base: env.VITE_CONTEXT || '/',
+        // plugins: An array of tools that extend Vite's core functionality
+        plugins: [
+            react(), // Enables React-specific features like Hot Module Replacement (HMR)
+            viteTsconfigPaths() // Ensures imports like '@/components/Button' work correctly
+        ],
 
-    // server: Configures the local development environment
-    server: {
-        port: 3000, // Optional: The port to run the development server on
-        open: true  // Optional: Automatically opens your browser when you run 'npm run dev'
-    },
+        // server: Configures the local development environment
+        server: {
+            port: 3000, // Optional: The port to run the development server on
+            open: true  // Optional: Automatically opens your browser when you run 'npm run dev'
+        },
+    }
 });
